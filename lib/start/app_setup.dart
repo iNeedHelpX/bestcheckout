d055a167a2882af4b0335a8d878227767a967869
Julia Pak <jpak0444@gmail.com>
2022-05-27 19:00:36 -0400

    genius

--- lib/start/app_setup.dart
+++ lib/start/app_setup.dart
@@ -29,38 +29,39 @@ class _AppSetupState extends State<AppSetup> {
         },
       ),
       bottomNavigationBar: BottomBar(
-          textStyle: TextStyle(fontWeight: FontWeight.bold),
-          selectedIndex: _currentPage,
-          onTap: (int index) {
-            _pageController.jumpToPage(index);
-            setState(() => _currentPage = index);
-          },
-          items: <BottomBarItem>[
-            BottomBarItem(
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-              activeColor: Colors.blue,
-              activeTitleColor: Colors.blue.shade600,
-            ),
-            BottomBarItem(
-              icon: Icon(Icons.favorite),
-              title: Text('Favorites'),
-              activeColor: Colors.red,
-            ),
-            BottomBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Account'),
-              backgroundColorOpacity: 0.1,
-              activeColor: Colors.greenAccent.shade700,
-            ),
-            BottomBarItem(
-              icon: Icon(Icons.settings),
-              title: Text('Settings'),
-              activeColor: Colors.orange,
-              activeIconColor: Colors.orange.shade600,
-              activeTitleColor: Colors.orange.shade700,
-            ),
-          ]),
+        textStyle: TextStyle(fontWeight: FontWeight.bold),
+        selectedIndex: _currentPage,
+        onTap: (int index) {
+          _pageController.jumpToPage(index);
+          setState(() => _currentPage = index);
+        },
+        items: <BottomBarItem>[
+          BottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+            activeColor: Colors.blue,
+            activeTitleColor: Colors.blue.shade600,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+            activeColor: Colors.red,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Account'),
+            backgroundColorOpacity: 0.1,
+            activeColor: Colors.greenAccent.shade700,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+            activeColor: Colors.orange,
+            activeIconColor: Colors.orange.shade600,
+            activeTitleColor: Colors.orange.shade700,
+          ),
+        ],
+      ),
     );
   }
 }
