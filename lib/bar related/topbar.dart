361d2e92dfe1ac63db2eb4d3c6b30f8b3edbafc0
Julia Pak <jpak0444@gmail.com>
2022-05-27 21:41:39 -0400

    bar

--- lib/bar related/topbar.dart
+++ lib/bar related/topbar.dart
@@ -1,5 +1,15 @@
+import 'package:bestcheckout/colors/bluepurple_gradient.dart';
 import 'package:flutter/material.dart';
 
 AppBar myPinkAppBar() {
-  return AppBar();
+  return AppBar(
+    title: Text("Best checkout", style: ,),
+    centerTitle: true,
+    toolbarHeight: 200,
+    flexibleSpace: Container(
+      decoration: BoxDecoration(
+        gradient: bluePurpleGrad(),
+      ),
+    ),
+  );
 }
