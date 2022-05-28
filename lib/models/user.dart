d2df45f30bd217a63ae45c94a374c0cf51633595
Julia Pak <jpak0444@gmail.com>
2022-05-28 04:56:46 -0400

    user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,29 @@
+import 'package:bestcheckout/models/cart.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class UserModel {
   static const ID = "id";
   static const NAME = "name";
   static const EMAIL = "email";
+  String id;
+  String name;
+  String email;
+  List<CartItemModel> cart;
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.cart,
+  });
+  List<CartItemModel> _convertCartItems(List cartFomDb) {
+    List<CartItemModel> _result = [];
+    if (cartFomDb.length > 0) {
+      cartFomDb.forEach((element) {
+        _result.add(CartItemModel.fromMap(element));
+      });
+    }
+    return _result;
+  }
+
+  List cartItemsToJson() => cart.map((item) => item.toJson()).toList();
 }
