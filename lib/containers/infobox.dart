088f0f467f759f8387623181075c03a160e0d19d
Julia Pak <jpak0444@gmail.com>
2022-05-29 01:01:00 -0400

    ok

---
+++ lib/containers/infobox.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InfoBox extends StatelessWidget {
+  final String boomTitle;
+  final String subtext;
+  const InfoBox({Key? key, required this.boomTitle, required this.subtext})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
