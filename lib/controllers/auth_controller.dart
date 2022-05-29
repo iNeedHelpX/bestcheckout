f32e119c8a75f336e5c739d1d6d76607a6508718
Julia Pak <jpak0444@gmail.com>
2022-05-29 17:05:00 -0400

    auth

---
+++ lib/controllers/auth_controller.dart
@@ -0,0 +1,98 @@
+import 'package:bestcheckout/constants/firebase_constants.dart';
+import 'package:bestcheckout/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class AuthController extends GetxController {
+  static AuthController instance = Get.find();
+  late Rx<User> firebaseUser;
+  RxBool isLoggedIn = false.obs;
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  String usersCollection = "users";
+  Rx<UserModel> userModel = UserModel().obs;
+
+  @override
+  void onReady() {
+    super.onReady();
+    firebaseUser = Rx<User>(auth.currentUser);
+    firebaseUser.bindStream(auth.userChanges());
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User user) {
+    if (user == null) {
+      Get.offAll(() => AuthenticationScreen());
+    } else {
+      userModel.bindStream(listenToUser());
+      Get.offAll(() => HomeScreen());
+    }
+  }
+
+  void signIn() async {
+    try {
+      showLoading();
+      await auth
+          .signInWithEmailAndPassword(
+              email: email.text.trim(), password: password.text.trim())
+          .then((result) {
+        _clearControllers();
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+      Get.snackbar("Sign In Failed", "Try again");
+    }
+  }
+
+  void signUp() async {
+    showLoading();
+    try {
+      await auth
+          .createUserWithEmailAndPassword(
+              email: email.text.trim(), password: password.text.trim())
+          .then((result) {
+        String _userId = result.user.uid;
+        _addUserToFirestore(_userId);
+        _clearControllers();
+      });
+    } catch (e) {
+      debugPrint(e.toString());
+      Get.snackbar("Sign In Failed", "Try again");
+    }
+  }
+
+  void signOut() async {
+    auth.signOut();
+  }
+
+  _addUserToFirestore(String userId) {
+    firebaseFirestore.collection(usersCollection).doc(userId).set({
+      "name": name.text.trim(),
+      "id": userId,
+      "email": email.text.trim(),
+      "cart": []
+    });
+  }
+
+  _clearControllers() {
+    name.clear();
+    email.clear();
+    password.clear();
+  }
+
+  updateUserData(Map<String, dynamic> data) {
+    logger.i("UPDATED");
+    firebaseFirestore
+        .collection(usersCollection)
+        .doc(firebaseUser.value.uid)
+        .update(data);
+  }
+
+  Stream<UserModel> listenToUser() => firebaseFirestore
+      .collection(usersCollection)
+      .doc(firebaseUser.value.uid)
+      .snapshots()
+      .map((snapshot) => UserModel.fromSnapshot(snapshot));
+}
